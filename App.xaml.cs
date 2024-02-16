1e0f48a9c16ca2198f13c395e3c1959aaf2f9f73
Philip Klare <philipklare@gmail.com>
2024-02-16 11:27:25 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wpf_Image_2024_02_09;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
